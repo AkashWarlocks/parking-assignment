164579f6de9cfc840bd9905613898f5920b55140
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-05-16 17:52:40 +0530

    deploy

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
+//Install express server
 const express = require('express');
-const allotParkingRoutes = require('./assignment_carparking/routes/allotparking.route')
-const bodyParser = require('body-parser');
+const path = require('path');
 
-const app = express ();
+const app = express();
 
-const port = process.env.PORT || 3000;
-app.use(
-    bodyParser.json()
-)
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/parking-assignmemt'));
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.get('/*', function(req,res) {
 
-
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,X-Frame-Options, Content-Type, Accept');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+res.sendFile(path.join(__dirname+'/dist/parking-assignmemt/index.html'));
 });
 
-app.use(allotParkingRoutes)
-
-app.listen(port, () => {
-  console.log('Server is up on port ' + port);
-});
-
-module.exports = app;
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
